8f2985e3c2b1c996c72fe43e7533b3197237438c
ahmoin <123082059+ahmoin@users.noreply.github.com>
2026-08-30 23:33:29 -0400

    Revert "feat: add Docker setup"
    This reverts commit 31bca19e843d0fe6a3ce2da21f9ba1316b2d9302.

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
-import path from "node:path";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@workspace/ui"],
-  output: "standalone",
-  outputFileTracingRoot: path.join(import.meta.dirname, "../../")
 };
 
 export default nextConfig;
